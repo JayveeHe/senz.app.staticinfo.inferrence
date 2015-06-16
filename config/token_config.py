03bd1fc1bf566c966aed1700e148e4ade6797bab
Jayveehe <jayveehe@gmail.com>
2015-06-16 23:22:46 +0800

    add env_setup.sh

--- config/token_config.py
+++ config/token_config.py
@@ -4,7 +4,7 @@ __author__ = 'Jayvee'
 import os
 
 filedir = os.path.dirname(__file__)
-token_config = json.loads(open('%s/config.json' % filedir, 'r').read())
+token_config = json.loads(open(r'%s/config.json' % filedir, 'r').read())
 # print token_config
 LOGENTRIES_TOKEN = token_config['LOGENTRIES_TOKEN']
 # ROLLBAR_TOKEN = ""
@@ -12,3 +12,9 @@ APP_ENV = token_config['APP_ENV']
 LEANCLOUD_APP_ID = token_config['LEANCLOUD_APP_ID']
 LEANCLOUD_APP_KEY = token_config['LEANCLOUD_APP_KEY']
 LEANCLOUD_APP_MASTER_KEY = token_config['LEANCLOUD_APP_MASTER_KEY']
+
+# init shell
+sh_output = open('%s/avoscloud_setup.sh' % filedir, 'w')
+shell_str = 'avoscloud add senz.analyzer.user.staticinfo.degree.dev %s\navoscloud deploy' % LEANCLOUD_APP_ID
+print shell_str
+sh_output.write(shell_str)
